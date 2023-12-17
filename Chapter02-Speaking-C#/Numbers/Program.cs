f92dc4b224e4f7518899674b0ac4eee5104a77b6
Yutong Li (Elton) <eltonyt.li@gmail.com>
2023-12-16 20:02:10 -0600

    middle of chapter 2

---
+++ Chapter02-Speaking-C#/Numbers/Program.cs
@@ -0,0 +1,4 @@
+﻿uint naturalNumber = 23;
+int integerNumber = -23;
+float realNumber = 2.3f;
+double anotherRealNumber = 2.3;
---
+++ Chapter02-Speaking-C#/Vocabulary/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Reflection;
+Console.WriteLine("Hello, World!");
+
+// #error version
+
+#region 
+
+//asdasd
+
+
+#endregion
+
+
+Console.WriteLine($"Compyuter named {Env.MachineName} says \"No.\"");
+
+System.Data.DataSet ds = new();
+HttpClient clinet = new();
+
+Assembly? myApp = Assembly.GetEntryAssembly();
+if (myApp is null) return;
+foreach (AssemblyName name in myApp.GetReferencedAssemblies())
+{
+    Assembly a = Assembly.Load(name);
+    int methodCount = 0;
+    foreach (TypeInfo t in a.DefinedTypes)
+    {
+        methodCount += t.GetMethods().Length;
+    }
+
+    WriteLine("{0:N0} types with {1:N0} methods in {2} assembly.",
+    arg0: a.DefinedTypes.Count(),
+    arg1: methodCount,
+    arg2: name.Name);
+}
