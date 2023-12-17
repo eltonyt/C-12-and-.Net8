79a0e9720e0942dd15579506e04b1f68fb41af2b
Yutong Li (Elton) <eltonyt.li@gmail.com>
2023-12-17 16:54:44 -0600

    Chapter 2 - Exercises finished

---
+++ Chapter02-Speaking-C#/Exercise/Ch02Ex03Numbers/Program.cs
@@ -0,0 +1,22 @@
+﻿WriteLine("------------------------------------------------------------------------------------------------------------------------");
+
+WriteLine("Type      Byte(s) of memory                                         Min                                              Max");
+
+WriteLine("------------------------------------------------------------------------------------------------------------------------");
+
+
+WriteLine($"sbyte    {sizeof(sbyte)}                                            {sbyte.MinValue}                                           {sbyte.MaxValue}");
+WriteLine($"byte    {sizeof(byte)}                                            {byte.MinValue}                                           {byte.MaxValue}");
+WriteLine($"short    {sizeof(short)}                                            {short.MinValue}                                           {short.MaxValue}");
+WriteLine($"ushort    {sizeof(ushort)}                                            {ushort.MinValue}                                           {ushort.MaxValue}");
+WriteLine($"int    {sizeof(int)}                                            {int.MinValue}                                           {int.MaxValue}");
+WriteLine($"uint    {sizeof(uint)}                                            {uint.MinValue}                                           {uint.MaxValue}");
+WriteLine($"long    {sizeof(long)}                                            {long.MinValue}                                           {long.MaxValue}");
+WriteLine($"ulong    {sizeof(ulong)}                                            {ulong.MinValue}                                           {ulong.MaxValue}");
+WriteLine($"Int128    {sizeof(Int128)}                                            {Int128.MinValue}                                           {Int128.MaxValue}");
+WriteLine($"UInt128    {sizeof(UInt128)}                                            {UInt128.MinValue}                                           {UInt128.MaxValue}");
+WriteLine($"Half    {sizeof(Half)}                                            {Half.MinValue}                                           {Half.MaxValue}");
+WriteLine($"float    {sizeof(float)}                                            {float.MinValue}                                           {float.MaxValue}");
+WriteLine($"double    {sizeof(double)}                                            {double.MinValue}                                           {double.MaxValue}");
+WriteLine($"decimal    {sizeof(decimal)}                                            {decimal.MinValue}                                           {decimal.MaxValue}");
+
